192df1253d3a35fbc138fb946d3a2f9aabb2679e
zen3t <josealvess6154@gmail.com>
2025-02-23 18:45:46 -0300

    Metodo GET Parametros de Rotas e Parametros de Query

--- main.py
+++ main.py
@@ -2,13 +2,24 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def home():
-    return {"message":"Hello World"}
+@app.get("/saudacao/{nome}")
+def home(nome: str):
+    text = f"Ola tudo bem {nome}"
+    return {"message": text}
 
 
-@app.get("/profile")
-async def profile():
-    return {"name":"Jose alves sobrinho neto"}
+@app.get('/quadrado/{numero}')
+def quadrado(numero: int):
+    resultado = numero * numero
+    text = f"O quadrado do numero {numero} e {resultado}"
+    return text
 
-
+@app.get("/valor")
+def dobro(valor:int):
+    resultado = 2 * valor
+    text = f" O dobro de {valor} e: {resultado}"
+    return text
+@app.get("/area_retangulo")
+def area_retangulo(largura:int,altura:int = 1):
+    area = altura * largura
+    return {'area': area}
