883d0f4bbd0d6574dc1cbf774927a37199851f5b
zen3t <josealvess6154@gmail.com>
2025-02-24 17:27:51 -0300

    Criando todos o metodos da API

--- AppAnimal/server.py
+++ AppAnimal/server.py
@@ -5,7 +5,7 @@ from uuid import uuid4
 app = FastAPI()
 
 class Animal(BaseModel):
-    id: Optional[int]
+    id: Optional[str]
     nome: str
     idade: int
     sexo: str
@@ -16,9 +16,29 @@ banco: List[Animal] = []
 @app.get('/animais')
 def lisar_animais():
     return banco
+@app.get("/animais/{animal_id}")
+def obter_animai(animal_id: str):
+    for animal in banco:
+        if animal.id == animal_id:
+            return animal
+    return {'erro': 'Animal nao localizado'}
 
+@app.delete('/animais/{animal_id}')
+def remover_animal(animal_id: str):
+    posicao = -1
+    # Buscar a posicao do  animal
+    for index, animal in enumerate(banco):
+        if animal.id == animal_id:
+            posicao = index
+            break
+    if posicao !=  -1:
+        banco.pop(posicao)
+        return {'mensagen':'Animal removido com sucesso!'}
+    else:
+        return {'erro': 'Animal nao localizado'}
+    
 @app.post('/animais')
 def criar_animais(animal:Animal):
     banco.append(animal)
-    animal.id = uuid4()
+    animal.id = str(uuid4())
     return None
